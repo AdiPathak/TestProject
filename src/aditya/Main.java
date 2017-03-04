7298161685872f5636026800e9baf17b7436ea7f
AdiPathak <apathak4239@gmail.com>
2017-03-04 18:33:51 -0500

    Added method spicyAss

--- src/aditya/Main.java
+++ src/aditya/Main.java
@@ -4,6 +4,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        spicyAss(4);
     }
+
+    private static int spicyAss(int a)
+    {
+        return 4 * a;
+    }
+
 }
