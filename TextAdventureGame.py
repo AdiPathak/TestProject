85724e5ebdb4f292c4f638b749752034507b8e90
AdiPathak <apathak4239@gmail.com>
2017-12-06 02:30:17 -0500

    Update TextAdventureGame.py

--- TextAdventureGame.py
+++ TextAdventureGame.py
@@ -11,8 +11,7 @@ def barn():
     print("You walk into the barn and you enter with the noise of all the animals")
     choice = int(input("Which animals would you like to go to first?\n1. Horses\n2. Cows\n3. Pigs\n4. Chickens"))
     if choice == 1:
-        print(
-            "You walk to the horses and feed them. They are very excited to see you. You start petting them and they really seem to like it.")
+        print("You walk to the horses and feed them. They are very excited to see you. You start petting them and they really seem to like it.")
         print("You then proceed to feed the chickens and making sure you collect the eggs. ")
         print("Then after feeding the chickens you go to the cows and feed them and make sure that you milk them")
         print("After milking the cows you head to the pigs and feed them while they squeal around waiting for their food.")
@@ -46,12 +45,18 @@ def lake():
     print("You peer into it and it happens to be a big tuna")
     choice = int(input("Do you let it go (1) or do you kill it and take it home with you took cook (2)."))
     if choice == 1:
-        print("You let the fish go and it swims back into the lake disappearing ")
+        print("You let the fish go and it swims back into the lake. ")
+    if choice == 2:
+        print("You take the slimy tuna out of the trap and it starts squirming around. You reach into your bag and take our your cutting knife.")
+        print("You pierce the fish, killing it and put it in your bad")
+        inventory.append("Big Tuna")
+    print("You decide to reset the trap again so you can catch another fish when you realize something strange in the water. It looks like something that you have not seen before.")
+
 
 #Local Variables
 
 gameLoop = True
-inventory = []
+inventory = ["Small cutting knife"]
 allFed = False
 fed = 0
 
@@ -81,15 +86,21 @@ while(gameLoop == True):
             print("You wake up to the birds chirping. Opening your eyes you see the sun peer through the tiny hole cut out into the wall.\n")
             print("Feeling thirsty again you head into your fridge and grab a water bottle and put it in your backpack and go outside ")
             inventory.append("Water Bottle")
-        choice = int(input("Where do you want to go?\n1. Barn\n2. Lake\n3. Go for a run\n4. Visit the village"))
-        if choice == 1:
+        choice_area = int(input("Where do you want to go?\n1. Barn\n2. Lake\n3. Go for a run\n4. Visit the village"))
+        if choice_area == 1:
             barn()
-        if choice == 2:
+        if choice_area == 2:
             lake()
-
-
-
-
+            choice_check = int(input("Do you check it out (1) or just leave it be (2)?"))
+            if choice_check == 1:
+                print("You leave your backpack on the ground and start swimming to the strange thing.")
+                time.sleep(7)
+                print("You then realize that the weird thing is a megalodon and it bites you and you die a very painful death. -_- *Guess they still do exist ")
+                time.sleep(1)
+                print("GAME OVER!")
+                gameLoop = False
+            if choice_check == 2:
+                print("You decide that its not worth time to check it out and you walk away and head back to your house.")
 
 
     if drink == 4:
